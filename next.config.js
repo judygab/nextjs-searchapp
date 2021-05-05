5d543f67bc235af33354a6109fcdb73f1408d94e
judygab <ivditigabeskiria@gmail.com>
2021-05-05 11:26:43 -0400

    added env variables

---
+++ next.config.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+module.exports = {
+  env: {
+    apiKey: process.env.API_KEY,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiKey: process.env.API_KEY, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiKey: process.env.API_KEY,
+  },
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import {useEffect, useState} from 'react'
 import Link from 'next/link'
+import getConfig from 'next/config'
+
+// Only holds serverRuntimeConfig and publicRuntimeConfig
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 
 export default function Home(initialData) {
   const [formInputs, setFormInputs] = useState({})
@@ -8,7 +12,7 @@ export default function Home(initialData) {
   const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(()=>{
-    setSearchResults(initialData.trendingMovies.data)
+    setSearchResults(initialData.trendingMovies.results)
   }, [initialData])
 
   const handleInputs = (event) => {
@@ -18,8 +22,7 @@ export default function Home(initialData) {
 
   const search = async (event) => {
     event.preventDefault()
-    //TODO: add url
-    let movies = await fetch(``)
+    let movies = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=1f6d5f7ad0f4eb427ec7134856eff45a&language=en-US&query=${formInputs.searchTerm}&page=1&include_adult=false`)
     movies = await movies.json()
     setSearchResults(movies.data)
     setSearchTerm(formInputs.searchTerm)
@@ -50,24 +53,22 @@ export default function Home(initialData) {
               </a>
       </Link>
 
-      {/* <div className="giphy-search-results-grid">
+       <div className="giphy-search-results-grid">
          {searchResults.map((each, index) => {
            return(
              <div key={index}>
                <h3>{each.title}</h3>
-               <img src={each.images.original.url} alt={each.title}/>
+               {/*<img src={each.images.original.url} alt={each.title}/>*/}
              </div>
            )
          })}
-       </div> */}
+       </div>
     </div>
   )
 }
 
 export async function getServerSideProps() {
-  //TODO add url
-  let trendingMovies = await fetch('')
+  let trendingMovies = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${serverRuntimeConfig.apiKey}`)
   trendingMovies = await trendingMovies.json()
-  console.log(trendingMovies);
   return {props: {trendingMovies: trendingMovies}}
 }
